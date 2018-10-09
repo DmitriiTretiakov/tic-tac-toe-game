c97a90989474adf080ecdbbfbbacefa24582e5fb
Dmitrii <tretiakov2511@gmail.com>
2018-10-09 10:52:51 +0300

    game

---
+++ src/main/java/com/tretiakov/app/App.java
@@ -0,0 +1,12 @@
+package com.tretiakov.app;
+
+public class App {
+    public static void main( String[] args ) {
+
+        GameLogic gameLogic = new GameLogic();
+        gameLogic.gameGycle();
+
+
+    }
+
+}
